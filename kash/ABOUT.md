ace80298690f18adbfdcb24bb37f245fbefe707c
kehiy <kehiiiiya@gmail.com>
2023-06-03 08:55:35 -0700

    kash

---
+++ kash/ABOUT.md
@@ -0,0 +1,3 @@
+#kash
+is a hash algorithm (maybe) thats not return fixed length strings
+thats return a 4 chars text from  8,000 lines data 
---
+++ kash/main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+)
+
+func kash(data string) string {
+	symbols := "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_-+={}[]/"
+	hash := 0
+	for i := 0; i < len(data); i++ {
+		hash = hash + int(data[i])
+	}
+	result := ""
+	for hash > 0 {
+		idx := hash % len(symbols)
+		result = string(symbols[idx]) + result
+		hash = hash / len(symbols)
+	}
+	return result
+}
+
+func main() {
+	fileBytes, err := ioutil.ReadFile("data.txt")
+	if err != nil {
+		panic(err)
+	}
+	fileString := string(fileBytes)
+	fmt.Println(kash(fileString))
+} 
