f0aad623e403933d9f4f409ba84ae2d69aee80c1
kehiy <kehiiiiya@gmail.com>
2023-06-07 10:01:04 -0700

    update random num python code

--- randomNumGen/random.py
+++ randomNumGen/random.py
@@ -1,15 +1,12 @@
 import time
 
-# get unix time stamp as seed
-# return each of numbers in timestamp to power of  3 + i and add to result
-def random():
-    seed = str(time.time())
-    result = 0
-    for i in seed:
-        if i == ".":
-            continue
-        result += int(i) ** 3 + int(i)
-    return result
+def gen_seed():
+    seed = int(str(time.time()).replace('.', '')[-8:])
+    return seed
+
+def random(seed : int, length : int = 10):
+    return str(int((int(str(seed)[-4:]) * seed / int(str(seed)[5:])) * (seed ** 8)))[-length:]
+    
 
 
-print(random()) # => 3504
+print(random(gen_seed(),1))
