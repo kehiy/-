4ef1106e83030b4082b0740f42f6ac3813997e26
kehiy <kehiiiiya@gmail.com>
2023-06-07 10:02:52 -0700

    add comment

--- randomNumGen/random.py
+++ randomNumGen/random.py
@@ -1,9 +1,13 @@
 import time
 
+# generates a seed from unix time stamp
 def gen_seed():
     seed = int(str(time.time()).replace('.', '')[-8:])
     return seed
 
+# return a pseudorandom number
+# seed : your seed number
+# length : your random number length
 def random(seed : int, length : int = 10):
     return str(int((int(str(seed)[-4:]) * seed / int(str(seed)[5:])) * (seed ** 8)))[-length:]
     
