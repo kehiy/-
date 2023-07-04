b65214936b48be5958571dfb0ee3a656d2402712
kehiy <kehiiiiya@gmail.com>
2023-07-04 11:40:44 -0500

    feat: dnscli done

---
+++ dnsCli/go.mod
@@ -0,0 +1,3 @@
+module github.com/kehiy/-/dnsCli
+
+go 1.20
---
+++ dnsCli/main.go
@@ -0,0 +1,64 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+)
+
+var (
+	DNS403 = []string{"10.202.10.202", "10.202.10.102"}
+	SHECAN = []string{"178.22.122.100", "185.51.200.2"}
+)
+
+func main() {
+	var list []string = []string{}
+	var err error
+
+	args := os.Args[1:]
+
+	for _, arg := range args {
+		if arg == "1" {
+			list = SHECAN
+			err = changeDNS(list)
+			if err != nil {
+				fmt.Printf("error in change dns: %v/n", err)
+			} else {
+				os.Exit(0)
+			}
+		} else if arg == "2" {
+			list = DNS403
+			err = changeDNS(list)
+			if err != nil {
+				fmt.Printf("error in change dns: %v/n", err)
+			} else {
+				os.Exit(0)
+			}
+		} else {
+			break
+		}
+	}
+
+	list = args
+	err = changeDNS(list)
+	if err != nil {
+		fmt.Printf("error in change dns: %v/n", err)
+	}
+	os.Exit(0)
+}
+
+func changeDNS(DNSs []string) error {
+	path := "/etc/resolv.conf"
+
+	newContent := "kehiy dns managee\n"
+	for _, dns := range DNSs {
+		newContent += fmt.Sprintf("nameserver %s\n", dns)
+	}
+
+	err := ioutil.WriteFile(path, []byte(newContent), 0644)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
