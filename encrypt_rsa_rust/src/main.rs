48f529ddffe950fac31cf6bf3f6133a3db6702cc
kehiy <kehiiiiya@gmail.com>
2023-07-18 17:44:12 +0800

    feat: rsa rust

---
+++ encrypt_rsa_rust/src/main.rs
@@ -0,0 +1,24 @@
+use rsa::{Pkcs1v15Encrypt, RsaPrivateKey, RsaPublicKey};
+
+
+fn main() {
+    let mut rng = rand::thread_rng();
+    let bits = 2048;
+    let priv_key = RsaPrivateKey::new(&mut rng, bits).expect("failed to generate a key");
+    let pub_key = RsaPublicKey::from(&priv_key);
+
+    let data = b"hello world";
+    let enc_data = pub_key.encrypt(&mut rng, Pkcs1v15Encrypt, &data[..]).expect("failed to encrypt");
+    assert_ne!(&data[..], &enc_data[..]);
+
+    let dec_data = priv_key.decrypt(Pkcs1v15Encrypt, &enc_data).expect("failed to decrypt");
+    assert_eq!(&data[..], &dec_data[..]);
+
+    // TODO: export as string []
+    println!("public key: {:?}", pub_key);
+    println!("private key: {:?}", priv_key);
+
+    println!("encrypt data: {:?}", enc_data);
+    println!("decrypt data: {:?}", dec_data);
+    println!("data: {:?}", data);
+}
