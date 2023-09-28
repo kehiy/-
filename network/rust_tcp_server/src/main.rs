ec6048bdc417dee3f4317471c1142ecd9570589a
Kay <kehiiiiya@gmail.com>
2023-09-29 01:49:36 +0800

    feat: rust tcp server

---
+++ network/rust_tcp_server/src/main.rs
@@ -0,0 +1,26 @@
+use std::net::{SocketAddrV4, Ipv4Addr, TcpListener};
+use std::io::{Read, Error};
+
+fn main() -> Result<(), Error> {
+    let loopback = Ipv4Addr::new(127, 0, 0, 1);
+
+    let socket = SocketAddrV4::new(loopback, 0);
+
+    let listener = TcpListener::bind(socket)?;
+
+    let port = listener.local_addr()?;
+
+    println!("Listening on {}, access this port to end the program", port);
+
+    let (mut tcp_stream, addr) = listener.accept()?; //block  until requested
+
+    println!("Connection received! {:?} is sending data.", addr);
+
+    let mut input = String::new();
+
+    let _ = tcp_stream.read_to_string(&mut input)?;
+
+    println!("{:?} says {}", addr, input);
+
+    Ok(())
+}
