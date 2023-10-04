5543c1eac482d70ba70caf3ae36932db7d01c454
Kay <kehiiiiya@gmail.com>
2023-10-05 05:41:08 +0800

    feat: rurand

--- lang/rust/rurand/target/debug/deps/rurand-5f569247559457e7.d
+++
@@ -1,5 +0,0 @@
-/home/keyhan/code/-/lang/rust/rurand/target/debug/deps/rurand-5f569247559457e7: src/main.rs
-
-/home/keyhan/code/-/lang/rust/rurand/target/debug/deps/rurand-5f569247559457e7.d: src/main.rs
-
-src/main.rs:
--- lang/rust/rurand/target/debug/rurand.d
+++
@@ -1,1 +0,0 @@
-/home/keyhan/code/-/lang/rust/rurand/target/debug/rurand: /home/keyhan/code/-/lang/rust/rurand/src/main.rs
