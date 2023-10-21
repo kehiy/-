fd98536b32acfbe514ffa4bcdd1b5001005a18e1
Kay <kehiiiiya@gmail.com>
2023-10-22 00:57:39 +0800

    refactor: rurep code

--- tools/rurep/src/main.rs
+++ tools/rurep/src/main.rs
@@ -1,14 +1,27 @@
 use std::{env, fs};
 
+struct Config {
+    query: String,
+    file_name: String,
+}
+
 fn main() {
     let args: Vec<String> = env::args().collect();
-    let query = &args[1];
-    let file_name = &args[2];
+    let config: Config = Config::new(&args);
 
-    println!("Searching for {:?}", query);
-    println!("In: {:?}", file_name);
+    println!("Searching for {:?}", config.query);
+    println!("In: {:?}", config.file_name);
 
-    let data = fs::read_to_string(file_name).expect("error while reading the file!");
+    let data = fs::read_to_string(config.file_name).expect("error while reading the file!");
 
     println!("file data:\n{}", data);
+}
+
+impl Config {
+    fn new(args: &[String]) -> Config {
+        let query = args[1].clone();
+        let file_name = args[2].clone();
+
+        Config { query, file_name }
+    }
 }
