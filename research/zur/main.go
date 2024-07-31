3d0ab81551ab77b2ecb41625a67862f9e0317901
kehiy <kehiiiiya@gmail.com>
2024-07-31 12:31:28 +0330

    zur enhance

--- research/zur/main.go
+++ research/zur/main.go
@@ -35,22 +35,14 @@ func isNumber(char string) bool {
 	if char == "" {
 		return false
 	}
-	n := []rune(char)[0]
-	if n >= '0' && n <= '9' {
-		return true
-	}
-	return false
+	return []rune(char)[0] >= '0' && []rune(char)[0] <= '9'
 }
 
 func isLetter(char string) bool {
 	if char == "" {
 		return false
 	}
-	n := []rune(char)[0]
-	if n >= 'a' && n <= 'z' {
-		return true
-	}
-	return false
+	return []rune(char)[0] >= 'a' && []rune(char)[0] <= 'z'
 }
 
 func tokenize(input string) []token {
