f6578a814c2b41ed715caeb5df01ed5d9d014d93
Kay <kehiiiiya@gmail.com>
2025-01-10 22:27:30 +0000

    adding slint nostr nip19 project.

---
+++ gui/learn/src/main.rs
@@ -0,0 +1,70 @@
+// Prevent console window in addition to Slint window in Windows release builds when, e.g., starting the app via file manager. Ignored on other platforms.
+#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
+
+use std::error::Error;
+
+use nostr::prelude::*;
+
+slint::include_modules!();
+
+fn decode(entity: &str) -> String {
+    let mut result: String = "".to_string();
+
+    let typ = Nip19::from_bech32(entity).unwrap();
+
+    if let Nip19::Pubkey(data) = typ {
+        result = data.to_hex();
+    }
+
+    if let Nip19::Profile(data) = &typ {
+        result = format!(
+            "pubkey: {}\nrelays: {:?}",
+            data.public_key.to_hex(),
+            data.relays
+        );
+    }
+
+    if let Nip19::Event(data) = &typ {
+        result = format!(
+            "author: {}\nrelays: {:?}\nid: {}",
+            data.author.unwrap().to_hex(),
+            data.relays,
+            data.event_id.to_hex()
+        );
+    }
+
+    if let Nip19::EventId(data) = &typ {
+        result = data.to_hex();
+    }
+
+    if let Nip19::Secret(data) = &typ {
+        result = data.to_secret_hex();
+    }
+
+    if let Nip19::Coordinate(data) = &typ {
+        result = format!(
+            "pubkey: {}\nrelays: {:?}",
+            data.public_key.to_hex(),
+            data.relays
+        );
+    }
+
+    result
+}
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let ui = AppWindow::new()?;
+
+    ui.on_decode({
+        let ui_handle = ui.as_weak();
+        move |entity| {
+            let ui = ui_handle.unwrap();
+            let result = decode(entity.as_str());
+            ui.set_results(result.into());
+        }
+    });
+
+    ui.run()?;
+
+    Ok(())
+}
--- research/kash(hash)/main.go
+++ research/kash(hash)/main.go
@@ -3,28 +3,40 @@ package main
 import (
 	"fmt"
 	"io/ioutil"
+	"os"
 )
 
-func kash(data string) string {
-	symbols := "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_-+={}[]/"
+var bitset = []byte{0x17, 0x27, 0x37, 0x47, 0x57, 0x67, 0x77}
+
+func kash(data string) int {
 	hash := 0
+
 	for i := 0; i < len(data); i++ {
-		hash = hash + int(data[i])
+		hash = hash + int(data[i]) >> 2
 	}
-	result := ""
+
+	resultArr := []byte{}
+
 	for hash > 0 {
-		idx := hash % len(symbols)
-		result = string(symbols[idx]) + result
-		hash = hash / len(symbols)
+		idx := hash % len(bitset)
+		resultArr = append(resultArr, bitset[idx])
+		hash = hash / len(bitset)
 	}
+
+	result := 0
+	for _, j := range resultArr {
+		result = result + int(j) | 1
+	}
+
 	return result
 }
 
 func main() {
-	fileBytes, err := ioutil.ReadFile("data.txt")
+	fileBytes, err := ioutil.ReadFile(os.Args[1])
 	if err != nil {
 		panic(err)
 	}
+
 	fileString := string(fileBytes)
 	fmt.Println(kash(fileString))
 }
