e7e641c939aeaa1e1036c8a79d8ea1eda6c6b552
Kay <kehiiiiya@gmail.com>
2025-01-27 18:28:55 +0000

    fix sce bech32.

--- research/sce/sce.py
+++ research/sce/sce.py
@@ -4,13 +4,13 @@
 import os
 import bech32
 
-#  from: https://asecuritysite.com/encryption/nprimes?y=256
+# from: https://asecuritysite.com/encryption/nprimes?y=256
 P = 90475314063646309186333911499545516020878393975510354007683035359907836935351
 G = 97196008581366143336110500868632826688558438691823781666953410301647280664993
 
-PRIVATE_KEY_PREFIX = "scepriv"
-PUBLIC_KEY_PREFIX = "scepub"
-SHARED_SECRET_PREFIX = "scesec"
+PRIVATE_KEY_PREFIX = "ss"
+PUBLIC_KEY_PREFIX = "sp"
+SHARED_SECRET_PREFIX = "sss"
 
 def generate_key_pair():
     random_bytes = os.urandom(32)
@@ -18,29 +18,30 @@ def generate_key_pair():
     pub = pow(G, priv, P)
     return priv, pub
 
-
 def get_secret(pub, priv):
     return pow(pub, priv, P)
+
+def int_to_bech32_data(n):
+    byte_array = n.to_bytes((n.bit_length() + 7) // 8, 'big')
+    return [b % 32 for b in byte_array]
 
 alice_priv, alice_pub = generate_key_pair()
 bob_priv, bob_pub = generate_key_pair()
 
-
-print("Alice Private Key:", bech32.bech32_encode(PRIVATE_KEY_PREFIX, alice_priv))
-print("Alice Public Key:", bech32.bech32_encode(PUBLIC_KEY_PREFIX, alice_pub))
+print("Alice Private Key:", bech32.bech32_encode(PRIVATE_KEY_PREFIX, int_to_bech32_data(alice_priv)))
+print("Alice Public Key:", bech32.bech32_encode(PUBLIC_KEY_PREFIX, int_to_bech32_data(alice_pub)))
 
 print("----------------------------------\n")
 
-
-print("Bob Private Key:", bech32.bech32_encode(PRIVATE_KEY_PREFIX, bob_priv))
-print("Bob Public Key:", bech32.bech32_encode(PUBLIC_KEY_PREFIX, bob_pub))
+print("Bob Private Key:", bech32.bech32_encode(PRIVATE_KEY_PREFIX, int_to_bech32_data(bob_priv)))
+print("Bob Public Key:", bech32.bech32_encode(PUBLIC_KEY_PREFIX, int_to_bech32_data(bob_pub)))
 
 print("----------------------------------\n")
 
 alice_sec = get_secret(bob_pub, alice_priv)
 bob_sec = get_secret(alice_pub, bob_priv)
 
-print("Bob Secret  ", bech32.bech32_encode(SHARED_SECRET_PREFIX, bob_sec))
-print("Alice Secret", bech32.bech32_encode(SHARED_SECRET_PREFIX, alice_sec))
+print("Bob Secret  ", bech32.bech32_encode(SHARED_SECRET_PREFIX, int_to_bech32_data(bob_sec)))
+print("Alice Secret", bech32.bech32_encode(SHARED_SECRET_PREFIX, int_to_bech32_data(alice_sec)))
 
 assert alice_sec == bob_sec
